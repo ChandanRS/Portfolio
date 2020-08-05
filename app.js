17b82ff5c3eaa4aa0745032962519206dd108eca
Chandan Shetti <chandanshetti149@gmail.com>
2020-08-05 16:57:20 +0530

    added project

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const navSlide = ()=>{
     const nav = document.querySelector('ul');
     const links = document.querySelectorAll('.nav-links li')
     const myInfo = document.querySelector('.my-info')
+    const contact = document.querySelector('.contact-container')
+    
 
     burgerButton.addEventListener('click',()=>{
         nav.classList.toggle('nav-active');
@@ -17,10 +19,14 @@ const navSlide = ()=>{
             link.style.animation  = `nav-text 0.5s ease forwards ${index / 7 + 0.3}s`
         })
 
-        if(myInfo.style.animation)
-        myInfo.style.animation='';
-        else
-        myInfo.style.animation = "myInfoSlide 0.5s ease forwards"
+        if(myInfo.style.animation && contact.style.animation){
+            myInfo.style.animation='';
+            contact.style.animation = ''
+        }
+        else{
+            myInfo.style.animation = "myInfoSlide 0.5s ease forwards"
+            contact.style.animation = "myInfoSlide 0.5s ease forwards"
+        }
         })
 }
 
